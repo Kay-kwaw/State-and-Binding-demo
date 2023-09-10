835839f3235f79a9e56718e33bc914f36ef49c67
kwaw kumi <kwawkumi9@gmail.com>
2023-09-10 21:45:47 +0000

    Initial Commit

---
+++ Product-demo/Product_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Product_demoApp.swift
+//  Product-demo
+//
+//  Created by kwaw-kumi on 10/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Product_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
